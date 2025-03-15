40eaec1ffb0b5a3613a8fdad998d3f79dc75f2a5
Henry Veedahl <henry.veedahl@outlook.com>
2025-03-15 11:59:01 -0500

    Finish Base Program

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 from names_generator import generate_name
 from tqdm import tqdm
 
-requestedTimes = 0;
-auctualTimes = 0;
+requestedTimes = 0
+actualTimes = 0
 
 while True:
     # Title Screen
@@ -23,21 +23,32 @@ while True:
     print("2: Open file location")
     print("3: Exit")
     command = input("User $~: ")
-
     #1: Generate file
     if command == "1":
         os.system('clear')
-        (int(requestedTimes)) = input("How many people to generate:")
-        while requestedTimes != auctualTimes:
-            fields = ['Name', 'DOB', 'Timestamp', 'Purchase Amount']
-            name = generate_name()
-            day = random.randint(1, 28)
-            mounth = random.randint(1, 12)
-            year = random.randint(1940, 2010)
-            dob = str(day) + "." + str(mounth) + "." + str(year)
-            hour = random.randint(0, 23)
-            minute = random.randint(0, 59)
-            timestamp = str(hour) + ":" + str(minute)
-            purchaseAmount = random.randint(50, 10000)
-            rows = [[name, dob, timestamp, purchaseAmount]]
-            filename = "Big Fatty.csv"
+        requestedTimes = int(input("How many people to generate: "))
+        filename = "Big Fatty.csv"
+        fields = ['Name', 'DOB', 'Timestamp', 'Purchase Amount']
+        pbar = tqdm(total=requestedTimes)
+        with open(filename, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(fields)
+            while actualTimes < requestedTimes:
+                name = generate_name()
+                day = random.randint(1, 28)
+                month = random.randint(1, 12)
+                year = random.randint(1940, 2010)
+                dob = f"{day}.{month}.{year}"
+                hour = random.randint(0, 23)
+                minute = random.randint(0, 59)
+                timestamp = f"{hour}:{minute}"
+                purchaseAmount = random.randint(50, 10000)
+                rows = [name, dob, timestamp, purchaseAmount]
+                csvwriter.writerow(rows)
+                actualTimes += 1
+                pbar.update(1)
+        pbar.close()
+    elif command == "2":
+        os.system('open .')
+    elif command == "3":
+        break
